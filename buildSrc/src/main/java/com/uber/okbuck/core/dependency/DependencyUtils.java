12235e4919bccc749e56947bb6e05d4f21abf4d4
Ravi Agarwal <ravi@uber.com>
2018-11-29 11:48:40 +0100

    Add support for downloading external dependencies via buck
    - Kotlin home, groovy home and robolectric dir is now being populated via a buck rule rather than actual jar files.
    - SHA256 is computed off the downloaded files via gradle. Support for getting sha256 from remote without downloading will be added next.
    - External Dependency api changes to return targetName and targetPath instead of cache name.
    
    Add support for automatically getting maven repositories
    
    Add support for local dependencies
    
    Fixes to make robolectric and hamcrest jar work
    
    Make kotlin_home work when not using versionless.
    
    Move maven repositories to a dictionary

--- buildSrc/src/main/java/com/uber/okbuck/core/dependency/DependencyUtils.java
+++ buildSrc/src/main/java/com/uber/okbuck/core/dependency/DependencyUtils.java
@@ -4,14 +4,13 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
 import com.uber.okbuck.core.util.FileUtil;
 import com.uber.okbuck.extension.ExternalDependenciesExtension;
 import com.uber.okbuck.extension.JetifierExtension;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.Objects;
@@ -75,6 +74,14 @@ public final class DependencyUtils {
     return FilenameUtils.isExtension(file.getName(), ALLOWED_EXTENSIONS);
   }
 
+  public static String sha256(File file) {
+    try {
+      return Files.asByteSource(file).hash(Hashing.sha256()).toString();
+    } catch (IOException e) {
+      throw new RuntimeException(String.format("Failed to calculate sha256 of %s", file));
+    }
+  }
+
   @Nullable
   static String getModuleClassifier(String fileNameString, @Nullable String version) {
     if (version == null) {
@@ -100,21 +107,6 @@ public final class DependencyUtils {
     } else {
       throw new IllegalStateException(
           String.format("Not a valid module filename %s", fileNameString));
-    }
-  }
-
-  @Nullable
-  static Path getContentPath(Path zipFilePath, String contentFileName) {
-    try {
-      FileSystem zipFile = FileSystems.newFileSystem(zipFilePath, null);
-      Path packagedPath = zipFile.getPath(contentFileName);
-      if (Files.exists(packagedPath)) {
-        return packagedPath;
-      } else {
-        return null;
-      }
-    } catch (IOException e) {
-      throw new RuntimeException(e);
     }
   }
 
@@ -163,7 +155,7 @@ public final class DependencyUtils {
                     && ((ModuleComponentIdentifier) identifier).getVersion().length() > 0) {
                   ModuleComponentIdentifier moduleIdentifier =
                       (ModuleComponentIdentifier) identifier;
-                  return ExternalDependency.from(
+                  return DependencyFactory.from(
                       moduleIdentifier.getGroup(),
                       moduleIdentifier.getModule(),
                       moduleIdentifier.getVersion(),
@@ -171,7 +163,7 @@ public final class DependencyUtils {
                       externalDependenciesExtension,
                       jetifierExtension);
                 } else {
-                  return ExternalDependency.fromLocal(
+                  return DependencyFactory.fromLocal(
                       artifact.getFile(), externalDependenciesExtension, jetifierExtension);
                 }
               })
